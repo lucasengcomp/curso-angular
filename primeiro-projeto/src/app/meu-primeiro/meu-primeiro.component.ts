aa2a712e65f5b23cf31ccab4dac62c6fd48d990b
Lucas Galvao <lucas.engcomp@outlook.com>
2022-11-16 14:52:43 -0300

    :sparkles: Angular #03: Hello, World! Criando primeiro projeto e o primeiro Componente

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'meu-primeiro',
+  template: `<p>Meu primeiro compoente com angular 2</p>`
+})
+export class MeuPrimeiroComponent {
+
+}
