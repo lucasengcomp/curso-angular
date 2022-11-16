b0d5384031b15b62fda05fa8100a79f966d80397
Lucas Galvao <lucas.engcomp@outlook.com>
2022-11-16 15:49:56 -0300

    :sparkles:  #05: Módulos (ngModule)

--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 
 
 @NgModule({
-  declarations: [CursosComponent],
+  declarations: [CursosComponent, CursoDetalheComponent],
   imports: [
     CommonModule
   ],
