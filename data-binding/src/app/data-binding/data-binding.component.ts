6a1b422882642a5cee49b228a1aedcef125b1379
Lucas Galvao <lucas.engcomp@outlook.com>
2022-11-20 08:52:18 -0300

    :tada: :sparkles: #09: Property Binding e Interpolação

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  imagemBob = '../../assets/bob.png';
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
